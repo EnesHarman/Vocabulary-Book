0752ec5daa08055ce990bd3a9b72ef6043689127
EnesHarman <enesharman60@gmail.com>
2023-02-11 23:06:20 +0300

    Save

--- src/files/Vocabularies.js
+++ src/files/Vocabularies.js
@@ -1,51 +1,26 @@
 const vocabularies = `Last (v) - (of a process, activity, or state) continue for a specified period of time - the guitar solo lasted for twenty minutes
-Decade (n) - A period of ten years - he taught at the university for nearly a decade
-Stretch (v) - Being capable of making longer without breaking - my jumper stretched in the wash
-Scatter (v) - Throw in various random directions - The chess pieces scatter over the floor.  
-Estimate (v) - Making prediction but stronger - the aim is to estimate the effects of macroeconomic policy on the economy
-Tilt (n) - A sloping position - It was challenging to drive because of the road’s tilt.
-Hemisphere (n) - A half of a sphere - 
+Stretch (v) - Being capable of making longer without breaking - my jumper stretched in the wash.  
 Fluctuation (n) - Something not stable - The fluctuation of food price in Turkey struggle citizens.
-Haze (n) - Fog(!) - There isn’t any haze today.
-Hazy (adj) - Covered by haze - The mountains were hazy. 
 Unwilling (adj) - Not eager to do something. - He was very unwilling to study because of that he failed his exam.
 Indicate (v) - Point out, show. - Future progressive tense is a verb tense that indicates something will occur in the future and continue for an expected length of time.
-Occur (v) - Happened - The exam occurred at 10 a.m.
-Gap (n) - A space between two objects. - many astronomers were hunting for an additional planet they thought should exist in the gap between the orbits of Mars and Jupiter.
-Observer (n) - A person who watches or notices something. - By 1890, more than 300 of these minor planets or asteroids had been discovered by sharp-eyed observers.
-Accelerate (v) - Being moved quickly - In that year, Max Wolf of Germany introduced astronomical photography to the search for asteroids, greatly accelerating the discovery of these dim objects.
-Distinguish (v) - Recognize as different -  And many of these foreign objects are located in the area of space which is commonly referred to as the asteroid belt, or the main belt, to distinguish this particular area in space from other asteroid populations like near-Earth asteroids and trojan asteroids.
-Alignment (v) - Arrangement in a straight line or correct relative positions. - the tiles had slipped out of alignment
+Distinguish (v) - Recognize as different -  He wears a blue belt to be distinguished from his brothers. 
 Influence (n) - Effect on something - Parents have an influence on their children about sports.
 Adverse (adj) - Ters, zıt - Adverts effects of parental involvement in sports
-Involvement (n) - İlişki - Parental involvement in sports
 Participation (n) - The action of taking part in something - Parents have a significant role in a young child’s level of enjoyment and participation.
 Significant (adj) -  Important to be worthy of attention - A significant increase in sales
-Contradict (verb) - Deny the truth of a statement by asserting the opposite - Actions that contradict with each other
+Contradict (verb) - Deny the truth of a statement by asserting the opposite - Actions that contradict each other
 Reveal (verb) - make (previously unknown or secret information) known to others - It is very like that Joe will reveal the things spoken around the table.
 Demanding (adj) - (of a task) requiring much skill or effort - Anne’s boos is very demanding.
 Relatively (adv) - Nispeten - She placed the vase on the shelf with relatively little effort.
-Facility (noun) - a place, amenity, or piece of equipment provided for a particular purpose - Cooking facilities 
-Vagabond (noun) - a person who wanders a place to place - My brother has spent his time living like a vagabond
 Wander (verb) - Walk or move in an aimless way - I wandered through narrow streets.
-Down and out (adj) - A person who has not money job or place to live. - A novel about being down and out in London.
 Outcast (noun) - A person who has been rejected by society - He felt like an outcast in his own family.
-Estranged (adj) - No longer close to someone - The couple became estranged and decided to divorce. 
 Poverty (noun)  - Not having enough money to meet basic needs. - The high unemployment rate has led to a significant increase in poverty 
-Alienate (verb) - Make someone feel isolated - The constant criticism from her boss had begun to alienate her from rest of the team
-Despair (noun) - The complete loss of hope - The news of the factory closure left people in despair. 
-Desperation (noun) - A state of despair. -  In desperation, he decided to sell everything he has to pay his debt. 
 Humiliate (verb) - Make someone feel foolish and injure their dignity and pride. - The cruel prank played on her by her classmates humiliated her in front of the entire school.
 Humiliation (noun) - The action of humiliating. - The athlete felt humiliation after losing the game. 
-Closure (noun) - an act or progress of closing. - The news of the factory closure made everyone upset. 
 Ordinary (adj) - With no special feature - Despite his ordinary appearance, he was a successful businessman.
 Customary (adj) - According to custom - These days, customary jobs aren't interested by younger people. 
-Unparalleled (adj) - Having no parallel or equal. - His unparalleled skills will carry this company to further.
 Phenomenal (adj) - Remarkable - He has a phenomenal appearance.
 Bizarre (adj) - Very strange, unusual.  - The bizarre behavior of the clown at the circus left the audience confused and disturbed.
-Habitual (adj) - As a habit - His habitual lateness was a source of frustration for his coworkers and boss.
-Exceptionally (adv) - to a greater degree than normal - She was an exceptionally talented musician. 
-Outstanding (adj) - Exceptionally good. - His outstanding performance in the sales department earned him a promotion and raise.
 Conventional (adj) - General, normal - The company decided to stick to conventional marketing strategies instead of experimenting with new ones. 
 Back [x] up (pv) - Support or defend something. - When the class was making fun of me, only the teacher back me up.
 Break down (pv) - Stop working, especially when referring to a machine. The washing machine broke down again.
@@ -56,74 +31,48 @@ Cheer [x] up (pv) - Make someone happy - Coding always cheers me up.
 Come around (pv) -  Change opinion or see a new point of view. - I never liked Germany, but I came up when I see their economy. 
 Come up (pv) - Approche or come near. - While I was talking with you,  a homeless person came up and requested some money.
 Count on (pv) - Trust or depend on someone - I am counting on you, my brother. 
-Dive into (pv) - Occupy oneself with something. - I’ll will dive into homework when I get home.
-End up (pv) - Eventually, reach some conclusion or destination - After 4 years of education, I finally ended up thinking what will I do. 
-Fill [x] up (pv) - Fill something completely - I need to fill the bottle up. Because we ran out of water.
 Find out (pv) - Discover or learn - I will find out who is behind this secret. 
 Get ahead (pv) - Succeed or progress - You will never get ahead in this company. 
 Get [x] back - Have something returned - Don’t worry. I will get your wallet back. 
-Cottage (n) - A small house - I live in a cottage in summer.
-Get [x] down (pv) - Depress or discourage someone - Kima always gets everyone down with her sad stories.
-Get into (pv) - Discuss something thoroughly. - We will get into this topic when we get home.
 Get together (pv) - Have a special event - The volleyball team is getting together for a dinner after training. 
 Give [x] away (pv) - Donate something or give something for free. - I gave my computer away.
-Give up (pv) - Accept defeat - I will never give up and get 7 points from IELTS.
 Give [x] up (pv) - Stop consuming or doing something - My girlfriend will give popeyes up for her diet. 
 Go against (pv) - Contradict, oppose, or fight against - A group of students went against the school dress code. 
-Go for (pv) - Try to attempt or to achieve something - I will go for a winning tournament. 
-Go over (pv) - Review or look at again - Marie went over the study guide last time before the exam.
 Hold [x] back (pv) - Prevent someone to do something - The hardness of IETLS will not hold me back from going to Germany.
 Keep [x] up (pv) - Continue doing something - I ought to keep working up if I want to make more money. 
 Leave [x] out (pv) - Omit or disregard something - Maria left the chart out from the presentation. 
-Let [x] down (pv) - Disappoint - You can trust me. I will never let you down.
-Let [x] in (pv) - Allow to enter - Don’t let him in. He has a knife!
-Let [x] know (pv) - Tell someone something - Let me know what’s going on here.
 Look after (pv) - Take care of someone or something - I will look after your brother, don’t worry.
-Look up to [x] (pv) - Admire or idolize someone - I look up to Atatürk to be a good person.
-Mix up (pv) - Confuse something with something else. - It’s easy to mix up Chris Pine and Chris Pratt.
-Pull [x] up (pv) - Retrieve or bring something near - Eugene pulled his document up his computer. 
 Run out of (pv) - Use all of the supply of something - We are running out of water.
 Set [x] up (pv) - Arrange or organize - I will set your desk up for you. 
-Shut [x] off (pv) - Turn off - Shut the machine of.
 Take after (pv) - Resemble, especially with parents. - Li takes after his father when it comes to politics. 
-Take [x] out (pv) - Move something outside - Take this garbage television out.
 Think [x] over (pv) - Consider something - When his parents suggested selling his Pokemon cards, Yosef thought it over. 
 Turn [x] down (pv) - Reject or say no - My crush turned me down after I asked them out. 
 To account for (pv) - To be the explanation or cause for something - ı can’t account for the missing money, I don’t know where it went!
-To allow for (pv)  - to take into consideration - I didn’t allow for traffic this morning. I am probably going to be late to work.
-To back off (pv)  - move backwards or become less aggressive - They decided to back off their threats after someone called the cops. 
-To back out of (pv)  - Withdraw from something you’ve agreed to or to undo a change - She decided to back out of vacation plans when she realized how much it cost. 
-To back up (pv)  - To move backwards - Everyone back up, give me some space.
 To block off (pv)  - To separate or close something - I need to block off a few hours on my schedule to check my emails.
-To blow over (pv)  - go to away, settle or calm down - I am waiting for the storm to blow over before I go on a hike.
 To break even (pv)  - To neither gain nor lose money - They thought they would make money on the sale, but they just broke even. 
 To bring up (pv)  - To mention - Don’t bring up her ex in conversation she’s still really upset about it.
 To call on (pv)  - To ask someone to do something - I know I can call on her when I need something.
-To check out (pv)  - To look or investigate - Did you check out the article I sent you? It was pretty interesting. 
-To fall for (pv)  - To be deceived by something - I can’t believe you fell for that scam. They’ll try to take your all money.
 To give way (pv)  - Collapse or break something under physical stress. - Be careful. I reckon the floor may give way!
-To hang on (pv)  - Waite a minute - Hang on! I’ll be right there. 
-To hold up (pv) - To delay or block the process 
-Interpret (verb) - Explain the meaning of information - To help interpret these signs, the Greeks relied on oracles, people who were believed to have the gift of prophecy and who served as intermediaries between the gods and humans.
-Rely (verb) - Depend on with full trust or confidence - The Greeks relied on oracles, people who were believed to have the gift of prophecy.
-Peer (verb) - To look carefully or with difficulty. - The ghost that roams the hallways and peers through the window. 
-Deduct (verb) - Subtract or take away (an amount or part) from a total. - Tax has been deducted from the payments 
-Homicide (noun) - The killing of one person by another. - He was charged with homicide.
-Convict (verb) - Declere someone to be guilty of a criminal offense - The thieves were convicted of the robbery
+Deduct (verb) - Subtract or take away (an amount or part) from a total. - Tax has been deducted from the payments
 Accountant (noun) - A person whose job is analyzing accounts. - Accountants are responsible for their client’s personal and professional information.
 Convey (verb) - Transport or carry to place, sometimes an idea - Develop and convey your idea.
 Persuade (verb) - Induce someone to do something through reasoning or argument. - It wasn’t easy, but I persuaded him to do the right thing.
 Persuasive (adj) - Good at persuading someone to believe or do something through reasoning or argument. - Persuasive Essay
 Coherent (adj) - Of an argument, being logical and consistent. - They’ve failed to develop a coherent economic strategy. 
 Tight-knit (adj) - A group of people bound together with strong relationships and common interests. - Small towns are often characterized by a tight-knit community where everyone knows each other and there is a strong sense of belonging.
-Tend (v) - Regularly or frequently behave in a particular way or have a certain characteristic. - People in small towns tend to lead a more relaxed and slower-paced life.
-Undergo (v) - Experience something, which is unpleasant - They have to undergo bad situations as they believe that good things might come out of it.
+Tend (verb) - Regularly or frequently behave in a particular way or have a certain characteristic. - People in small towns tend to lead a more relaxed and slower-paced life.
+Undergo (verb) - Experience something, which is unpleasant - They have to undergo bad situations as they believe that good things might come out of it.
 Hence (adv) - As a consequence - Hence, this explains why some people choose to accept bad situations. 
 Paranoid (adj) - Unreasonably, or obsessively suspicious. - They feel a bit paranoid that the situation will become worse if they do not do so.
 Throughout (prep) - In every part of something (like text) - Shows a clear position throughout the response.
 Entertainment (noun) - The action providing amusement or enjoyment. - Big cities offer a wider range of entertainment. 
 Diverse (adj) - Showing a great deal of variety. - Big cities are more diverse. 
 Merit (noun) - The quality of being particularly good or worthy.  - Both ideas have their merits.
+Influence (verb) - Affect something. - I don't believe I can influence the situation
+Resemble (verb) - Have a similar appearance - The first matches resembling those used today were made in 1827.
+Notorious (adj) - Famous or well-known, typically for some bad quality. 
+Encourage (verb) - Give support or confidence. - Parents should encourage their children to participate
+Intact (adj) - Not damaged or impaired in any way. - In general, your aim is to leave the site intact.  
 `;
 
 export default vocabularies;
