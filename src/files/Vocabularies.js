a60aa6485bad160851bc02af5b599ac1b4bf0c53
Enes Harman <enes@Eness-MacBook-Pro.local>
2023-02-19 21:40:23 +0300

    Add new vocabularies

--- src/files/Vocabularies.js
+++ src/files/Vocabularies.js
@@ -1,61 +1,26 @@
-const vocabularies = `Last (v) - (of a process, activity, or state) continue for a specified period of time - the guitar solo lasted for twenty minutes
-Stretch (v) - Being capable of making longer without breaking - my jumper stretched in the wash.  
-Fluctuation (n) - Something not stable - The fluctuation of food price in Turkey struggle citizens.
-Unwilling (adj) - Not eager to do something. - He was very unwilling to study because of that he failed his exam.
+const vocabularies = `Fluctuation (n) - Something not stable - The fluctuation of food price in Turkey struggle citizens.
 Indicate (v) - Point out, show. - Future progressive tense is a verb tense that indicates something will occur in the future and continue for an expected length of time.
 Distinguish (v) - Recognize as different -  He wears a blue belt to be distinguished from his brothers. 
 Influence (n) - Effect on something - Parents have an influence on their children about sports.
 Adverse (adj) - Ters, zıt - Adverts effects of parental involvement in sports
 Participation (n) - The action of taking part in something - Parents have a significant role in a young child’s level of enjoyment and participation.
 Significant (adj) -  Important to be worthy of attention - A significant increase in sales
-Contradict (verb) - Deny the truth of a statement by asserting the opposite - Actions that contradict each other
-Reveal (verb) - make (previously unknown or secret information) known to others - It is very like that Joe will reveal the things spoken around the table.
 Demanding (adj) - (of a task) requiring much skill or effort - Anne’s boos is very demanding.
 Relatively (adv) - Nispeten - She placed the vase on the shelf with relatively little effort.
-Wander (verb) - Walk or move in an aimless way - I wandered through narrow streets.
 Outcast (noun) - A person who has been rejected by society - He felt like an outcast in his own family.
 Poverty (noun)  - Not having enough money to meet basic needs. - The high unemployment rate has led to a significant increase in poverty 
-Humiliate (verb) - Make someone feel foolish and injure their dignity and pride. - The cruel prank played on her by her classmates humiliated her in front of the entire school.
-Humiliation (noun) - The action of humiliating. - The athlete felt humiliation after losing the game. 
 Ordinary (adj) - With no special feature - Despite his ordinary appearance, he was a successful businessman.
 Customary (adj) - According to custom - These days, customary jobs aren't interested by younger people. 
 Phenomenal (adj) - Remarkable - He has a phenomenal appearance.
-Bizarre (adj) - Very strange, unusual.  - The bizarre behavior of the clown at the circus left the audience confused and disturbed.
 Conventional (adj) - General, normal - The company decided to stick to conventional marketing strategies instead of experimenting with new ones. 
 Back [x] up (pv) - Support or defend something. - When the class was making fun of me, only the teacher back me up.
-Break down (pv) - Stop working, especially when referring to a machine. The washing machine broke down again.
-Call around (pv) - Contact many people. - He called around to find someone. 
-Call [x] of (pv) - Cancel something - We called the meeting of yesterday.
-Check [x] of (pv) - Verify something or a person - Check his ID of.  
-Cheer [x] up (pv) - Make someone happy - Coding always cheers me up. 
-Come around (pv) -  Change opinion or see a new point of view. - I never liked Germany, but I came up when I see their economy. 
-Come up (pv) - Approche or come near. - While I was talking with you,  a homeless person came up and requested some money.
-Count on (pv) - Trust or depend on someone - I am counting on you, my brother. 
-Find out (pv) - Discover or learn - I will find out who is behind this secret. 
-Get ahead (pv) - Succeed or progress - You will never get ahead in this company. 
-Get [x] back - Have something returned - Don’t worry. I will get your wallet back. 
-Get together (pv) - Have a special event - The volleyball team is getting together for a dinner after training. 
-Give [x] away (pv) - Donate something or give something for free. - I gave my computer away.
-Give [x] up (pv) - Stop consuming or doing something - My girlfriend will give popeyes up for her diet. 
-Go against (pv) - Contradict, oppose, or fight against - A group of students went against the school dress code. 
-Hold [x] back (pv) - Prevent someone to do something - The hardness of IETLS will not hold me back from going to Germany.
-Keep [x] up (pv) - Continue doing something - I ought to keep working up if I want to make more money. 
-Leave [x] out (pv) - Omit or disregard something - Maria left the chart out from the presentation. 
-Look after (pv) - Take care of someone or something - I will look after your brother, don’t worry.
-Run out of (pv) - Use all of the supply of something - We are running out of water.
-Set [x] up (pv) - Arrange or organize - I will set your desk up for you. 
-Take after (pv) - Resemble, especially with parents. - Li takes after his father when it comes to politics. 
-Think [x] over (pv) - Consider something - When his parents suggested selling his Pokemon cards, Yosef thought it over. 
-Turn [x] down (pv) - Reject or say no - My crush turned me down after I asked them out. 
-To account for (pv) - To be the explanation or cause for something - ı can’t account for the missing money, I don’t know where it went!
-To block off (pv)  - To separate or close something - I need to block off a few hours on my schedule to check my emails.
 To break even (pv)  - To neither gain nor lose money - They thought they would make money on the sale, but they just broke even. 
 To bring up (pv)  - To mention - Don’t bring up her ex in conversation she’s still really upset about it.
 To call on (pv)  - To ask someone to do something - I know I can call on her when I need something.
 To give way (pv)  - Collapse or break something under physical stress. - Be careful. I reckon the floor may give way!
 Deduct (verb) - Subtract or take away (an amount or part) from a total. - Tax has been deducted from the payments
 Accountant (noun) - A person whose job is analyzing accounts. - Accountants are responsible for their client’s personal and professional information.
-Convey (verb) - Transport or carry to place, sometimes an idea - Develop and convey your idea.
+Convey (verb) - Transport or carry to place, sometimes an idea, iletmek - Develop and convey your idea.
 Persuade (verb) - Induce someone to do something through reasoning or argument. - It wasn’t easy, but I persuaded him to do the right thing.
 Persuasive (adj) - Good at persuading someone to believe or do something through reasoning or argument. - Persuasive Essay
 Coherent (adj) - Of an argument, being logical and consistent. - They’ve failed to develop a coherent economic strategy. 
@@ -63,7 +28,6 @@ Tight-knit (adj) - A group of people bound together with strong relationships an
 Tend (verb) - Regularly or frequently behave in a particular way or have a certain characteristic. - People in small towns tend to lead a more relaxed and slower-paced life.
 Undergo (verb) - Experience something, which is unpleasant - They have to undergo bad situations as they believe that good things might come out of it.
 Hence (adv) - As a consequence - Hence, this explains why some people choose to accept bad situations. 
-Paranoid (adj) - Unreasonably, or obsessively suspicious. - They feel a bit paranoid that the situation will become worse if they do not do so.
 Throughout (prep) - In every part of something (like text) - Shows a clear position throughout the response.
 Entertainment (noun) - The action providing amusement or enjoyment. - Big cities offer a wider range of entertainment. 
 Diverse (adj) - Showing a great deal of variety. - Big cities are more diverse. 
@@ -71,7 +35,6 @@ Merit (noun) - The quality of being particularly good or worthy.  - Both ideas h
 Influence (verb) - Affect something. - I don't believe I can influence the situation
 Resemble (verb) - Have a similar appearance - The first matches resembling those used today were made in 1827.
 Notorious (adj) - Famous or well-known, typically for some bad quality. 
-Encourage (verb) - Give support or confidence. - Parents should encourage their children to participate
 Intact (adj) - Not damaged or impaired in any way. - In general, your aim is to leave the site intact.  
 Obscure (adj) - Not discovered or known about - However, the origins word of Viking, itself, are obscure. 
 Exaggerated (adj) - Represented as larger, better - Viking conquests are exaggerated to bolster the country’s ego after a territories loss.
@@ -87,7 +50,7 @@ Uninhabitable (adj) - A place that is not suitable for living. - If the world be
 Furthermore (adv) - In addition, besides. - Furthermore, our descendants could continue to enjoy the company of other species. 
 Convenient (adj) - Fitting in well with a person's needs, activities, and plans. -  Leaving only the ones that we find convenient
 Magnitude (noun) - Great size. - Yellowstone National Park in the USA occupies the caldera of an exceedingly ancient volcano of extraordinary magnitude
-Predispose (verb) - Make someone liable - Certain kinds of political systems and strategies would predispose us to long-term survival. 
+Predispose (verb) - Make someone liable (yatkın) - Certain kinds of political systems and strategies would predispose us to long-term survival. 
 Conceal (verb) - Hide - Recipes to conceal facial defects caused by aging. 
 Abundance (noun) - A very large of quantity of something - Evidence exists in abundance showing cosmetics use in ancient times. 
 Solely (adv) - Only - Long hair as a symbol of beauty was worn solely by women of ancient culture.  
@@ -97,6 +60,12 @@ Coveted (adj) - Greatly desired or envied - Perfumes were prestigious products k
 Appease (verb) - Pacify or placate someone (gods) - Cosmetics were also connected with cultic worship and witchcraft: to appease the various gods. 
 Anoint (verb) - Rub with oil, typically as part of a religious ceremony. - They used to care for the body with substances that softened the skin and anoint it with fragrant oils and ointments.
 Inflammation (noun) - A localized physical condition in which part of the body becomes reddened, swollen, hot, and often painful. - The eye-paint repelled the little flies that transmitted eye inflammations.
+Debated (adj) - Tartışılan - A highly debated topic.
+Prioritize (verb) - Öncelik vermek - Young people should prioritize their education and health. 
+Towards (prep) - In direction of - This fantasy is helping her take her first steps towards her capacity for creativity.
+Underpin (verb) - To support or strengthen something, often something abstract - It underpins how we develop as problem solving adults. 
+Extol (verb) - Praise , Övmek - Plato extolled its virtues as a means of developing skills for adult life. 
+Scarce (adj) - Nadir - The opportunities for free play are becoming increasingly scarce. 
 `;
 
 export default vocabularies;
