a69d906045e49ab7c2fd8c3015705857afaaa13a
Enes Harman <enes@Eness-MacBook-Pro.local>
2023-02-24 22:43:23 +0300

    Add new vocabularies

--- src/files/Vocabularies.js
+++ src/files/Vocabularies.js
@@ -1,28 +1,20 @@
 const vocabularies = `Fluctuation (n) - Something not stable - The fluctuation of food price in Turkey struggle citizens.
 Indicate (v) - Point out, show. - Future progressive tense is a verb tense that indicates something will occur in the future and continue for an expected length of time.
-Distinguish (v) - Recognize as different -  He wears a blue belt to be distinguished from his brothers. 
 Influence (n) - Effect on something - Parents have an influence on their children about sports.
 Adverse (adj) - Ters, zıt - Adverts effects of parental involvement in sports
 Participation (n) - The action of taking part in something - Parents have a significant role in a young child’s level of enjoyment and participation.
 Significant (adj) -  Important to be worthy of attention - A significant increase in sales
 Demanding (adj) - (of a task) requiring much skill or effort - Anne’s boos is very demanding.
 Relatively (adv) - Nispeten - She placed the vase on the shelf with relatively little effort.
-Outcast (noun) - A person who has been rejected by society - He felt like an outcast in his own family.
-Poverty (noun)  - Not having enough money to meet basic needs. - The high unemployment rate has led to a significant increase in poverty 
 Ordinary (adj) - With no special feature - Despite his ordinary appearance, he was a successful businessman.
 Customary (adj) - According to custom - These days, customary jobs aren't interested by younger people. 
 Phenomenal (adj) - Remarkable - He has a phenomenal appearance.
 Conventional (adj) - General, normal - The company decided to stick to conventional marketing strategies instead of experimenting with new ones. 
-Back [x] up (pv) - Support or defend something. - When the class was making fun of me, only the teacher back me up.
-To break even (pv)  - To neither gain nor lose money - They thought they would make money on the sale, but they just broke even. 
-To bring up (pv)  - To mention - Don’t bring up her ex in conversation she’s still really upset about it.
-To call on (pv)  - To ask someone to do something - I know I can call on her when I need something.
-To give way (pv)  - Collapse or break something under physical stress. - Be careful. I reckon the floor may give way!
 Deduct (verb) - Subtract or take away (an amount or part) from a total. - Tax has been deducted from the payments
 Accountant (noun) - A person whose job is analyzing accounts. - Accountants are responsible for their client’s personal and professional information.
-Convey (verb) - Transport or carry to place, sometimes an idea, iletmek - Develop and convey your idea.
 Persuade (verb) - Induce someone to do something through reasoning or argument. - It wasn’t easy, but I persuaded him to do the right thing.
 Persuasive (adj) - Good at persuading someone to believe or do something through reasoning or argument. - Persuasive Essay
+Perceive (verb) - Become aware or conscious of something - Vikings have been perceived in numerous different ways.
 Coherent (adj) - Of an argument, being logical and consistent. - They’ve failed to develop a coherent economic strategy. 
 Tight-knit (adj) - A group of people bound together with strong relationships and common interests. - Small towns are often characterized by a tight-knit community where everyone knows each other and there is a strong sense of belonging.
 Tend (verb) - Regularly or frequently behave in a particular way or have a certain characteristic. - People in small towns tend to lead a more relaxed and slower-paced life.
@@ -39,10 +31,10 @@ Intact (adj) - Not damaged or impaired in any way. - In general, your aim is to 
 Obscure (adj) - Not discovered or known about - However, the origins word of Viking, itself, are obscure. 
 Exaggerated (adj) - Represented as larger, better - Viking conquests are exaggerated to bolster the country’s ego after a territories loss.
 Bolster (verb) - Support or strengthen - Viking conquests are exaggerated to bolster the country’s ego after a territories loss.
-Perceive (verb) - Become aware or conscious of something - Vikings have been perceived in numerous different ways.
 Vilify (verb) - Speak or write about in an abusively disparaging manner. -  Vilified as conquerors and romanticised as adventurers.
 Subsequently (adverb) - After a  particular things has happened. - Vikings sailed to England to storm coastal monasteries, and subsequently, large swathes of England fell under Viking rule.
 Dispute (noun) - A disagreement on an argument - How much territory Vikings controlled is also in dispute. 
+Despite (prep) - Aksine - Despite this recognition of the importance of employee development, the hospitality industry has historically been dominated by underdeveloped HR practices
 Initially (adv) - At first - Initially, Vikings were polytheists, believing in many gods. 
 Forebear (noun) - An ancestor -  He was adamant it was his forebears and not the Swedes’ or Danes’ who had colonised Iceland, Greenland, and Vinland, in what is now Canada.
 Eruption (noun) - An act or instance of erupting - An eruption by Yellowstone is likely to be more destructive than previous volcanic eruptions.
@@ -55,10 +47,9 @@ Conceal (verb) - Hide - Recipes to conceal facial defects caused by aging.
 Abundance (noun) - A very large of quantity of something - Evidence exists in abundance showing cosmetics use in ancient times. 
 Solely (adv) - Only - Long hair as a symbol of beauty was worn solely by women of ancient culture.  
 Establish (verb) - Set up on a permanent basis. - The Egyptian Queen Hatsheput sent a royal expedition to Punt to establish a trade route for myrrh.
-Attitude (verb) - A settled way of thinking or feeling something - Its interesting to compare attitudes. 
+Attitude (noun) - A settled way of thinking or feeling something - Its interesting to compare attitudes. 
 Coveted (adj) - Greatly desired or envied - Perfumes were prestigious products known throughout the ancient world and coveted by kings and princes.
 Appease (verb) - Pacify or placate someone (gods) - Cosmetics were also connected with cultic worship and witchcraft: to appease the various gods. 
-Anoint (verb) - Rub with oil, typically as part of a religious ceremony. - They used to care for the body with substances that softened the skin and anoint it with fragrant oils and ointments.
 Inflammation (noun) - A localized physical condition in which part of the body becomes reddened, swollen, hot, and often painful. - The eye-paint repelled the little flies that transmitted eye inflammations.
 Debated (adj) - Tartışılan - A highly debated topic.
 Prioritize (verb) - Öncelik vermek - Young people should prioritize their education and health. 
@@ -75,6 +66,30 @@ Remedy (noun) - Çare - There is no easy remedy for the environmental damage cau
 Fact (noun) - Durum, gerçek, olgu - The fact is, we can't afford to buy a new car right now.
 Narrow (noun) - Daralmak - We need to narrow our focus and concentrate on the most important issues.
 Income (noun) - Gelir - he was pleased to receive a raise in her income after working at the company for a year.
+Growing concern (noun) - Artan endişe - The widening gap between rich and poor is a growing concern in many countries around the world. 
+As a whole (noun) - Bütün olarak - This trend can have serious implications for the well-being of individuals and society as a whole.
+Well-being (noun) - Refah - This trend can have serious implications for the well-being of individuals and society as a whole.
+Fund (noun) - Sermaye - The revenue generated from these taxes could then be used to fund programs and services for those in need.
+Wealth (noun) - Varlık, Zenginlik - When a small percentage of the population controls the majority of the wealth, it can create a sense of injustice and resentment among those who are less fortunate.
+Leaflet (noun) - Broşür - They also distributed leaflets describing the danger of cars and inviting people to use the white bikes.  
+Particularly (adv) - Özellikle -  Particularly when it came to publicizing Provo’s aims.
+Initiative (noun) - Girişim - The police were opposed to Provo’s initiatives and almost as soon as the white bikes were distributed around the city, they removed them.
+Seize (verb) - Ele geçirmek (Fırsatı) - Schimmelpennink seized this opportunity to present a more elaborate Witte Fietsenplan to the city council.
+Unanimously (adv) - Oy birliği ile - Nevertheless, the council unanimously rejected the plan.
+Arouse (verb) - Canlandırmak, uyandırmak (bir fikri) - He succeeded in arousing the interest of the Dutch Ministry of Transport.
+Decisive (adj) - Belirleyici - That was a decisive moment in the history of bike-sharing
+Ingredient (noun) - Bileşen - A critical ingredient in success of hotels is developing and maintaining superior performance from their employees. 
+Accomplished (adj) - Başarılı - How is that accomplished? / Bu nasıl başarılır?
+Invest (verb) - Yatırım yapmak - What Human Resource Management (HRM) practices should organizations invest in to acquire and retain great employees?
+Retain (verb) - Sürdürmek - Yatırım yapmak - What Human Resource Management (HRM) practices should organizations invest in to acquire and retain great employees?
+Aim (verb) - Odaklanmak - Some hotels aim to provide superior working conditions for their employees.
+By contrast, (other) - Karşıt olarak - By contrast, the service sector, and more specifically hotels, has traditionally not extended these practices to address basic employee needs, such as good working conditions.
+In order to succeed (other) - Başarılı olmak için - Pfeffer emphasizes that in order to succeed in a global business environment, organizations must make investment in Human Resource Management.
+Substance (noun) - Öz - The substance of HRM practices 
+Recurring (adj) - Yinelenen - In addition, or maybe as a result, high employee turnover has been a recurring problem throughout the hospitality industry
+Succinctly (adv) - Kısaca - This was succinctly summarized by Michel
+Inherently (adv) - Doğal olarak - These conditions are inherently linked to the working environment.
+It is therefore (other) - Bu nedenle - It is therefore essential for hotel management to develop HRM practices that enable them to inspire and retain competent employees. 
 `;
 
 export default vocabularies;
