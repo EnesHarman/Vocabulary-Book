57f059faa8b900647e66f60f0b9f81179c13a96f
Enes Harman <enes@Eness-MacBook-Pro.local>
2023-02-26 17:36:08 +0300

    Add new vocabularies

--- src/files/Vocabularies.js
+++ src/files/Vocabularies.js
@@ -51,45 +51,43 @@ Attitude (noun) - A settled way of thinking or feeling something - Its interesti
 Coveted (adj) - Greatly desired or envied - Perfumes were prestigious products known throughout the ancient world and coveted by kings and princes.
 Appease (verb) - Pacify or placate someone (gods) - Cosmetics were also connected with cultic worship and witchcraft: to appease the various gods. 
 Inflammation (noun) - A localized physical condition in which part of the body becomes reddened, swollen, hot, and often painful. - The eye-paint repelled the little flies that transmitted eye inflammations.
-Debated (adj) - Tartışılan - A highly debated topic.
-Prioritize (verb) - Öncelik vermek - Young people should prioritize their education and health. 
+Debate (verb) - To discuss or argue a topic or issue - The students debated about social media
+Prioritize (verb) - Decide which thing is more important to be done first- Young people should prioritize their education and health. 
 Towards (prep) - In direction of - This fantasy is helping her take her first steps towards her capacity for creativity.
 Underpin (verb) - To support or strengthen something, often something abstract - It underpins how we develop as problem solving adults. 
 Extol (verb) - Praise , Övmek - Plato extolled its virtues as a means of developing skills for adult life. 
-Scarce (adj) - Nadir - The opportunities for free play are becoming increasingly scarce. 
-Curtail (verb) - Kısıtlamak - Outdoor play is curtailed by perceptions of risk to do with traffic, as well as parent’ increased wish to protect their children.
+Scarce (adj) - Rare - The opportunities for free play are becoming increasingly scarce. 
+Curtail (verb) - Reduce or limit something - Outdoor play is curtailed by perceptions of risk to do with traffic, as well as parent’ increased wish to protect their children.
 Concerned (adj) - Worried - International bodies like United Nations and the European Union have begun yo develop policies concerned with children’s right to play. 
-Drawback (noun) - Eksi yön, sakınca, engel - One possible drawback of using the internet for education is leading to copying homework.
-Enhance (verb) - Geliştirmek - Using the internet for education has a big advantage which lets students to enhance their studies.
-Taking precautions (verb) - Önlem almak - To ensure our safety, we took precautions by wearing helmets and safety gear while riding our bikes.
-Remedy (noun) - Çare - There is no easy remedy for the environmental damage caused by industrial pollution.
-Fact (noun) - Durum, gerçek, olgu - The fact is, we can't afford to buy a new car right now.
-Narrow (noun) - Daralmak - We need to narrow our focus and concentrate on the most important issues.
-Income (noun) - Gelir - he was pleased to receive a raise in her income after working at the company for a year.
-Growing concern (noun) - Artan endişe - The widening gap between rich and poor is a growing concern in many countries around the world. 
-As a whole (noun) - Bütün olarak - This trend can have serious implications for the well-being of individuals and society as a whole.
-Well-being (noun) - Refah - This trend can have serious implications for the well-being of individuals and society as a whole.
-Fund (noun) - Sermaye - The revenue generated from these taxes could then be used to fund programs and services for those in need.
-Wealth (noun) - Varlık, Zenginlik - When a small percentage of the population controls the majority of the wealth, it can create a sense of injustice and resentment among those who are less fortunate.
-Leaflet (noun) - Broşür - They also distributed leaflets describing the danger of cars and inviting people to use the white bikes.  
-Particularly (adv) - Özellikle -  Particularly when it came to publicizing Provo’s aims.
-Initiative (noun) - Girişim - The police were opposed to Provo’s initiatives and almost as soon as the white bikes were distributed around the city, they removed them.
-Seize (verb) - Ele geçirmek (Fırsatı) - Schimmelpennink seized this opportunity to present a more elaborate Witte Fietsenplan to the city council.
-Unanimously (adv) - Oy birliği ile - Nevertheless, the council unanimously rejected the plan.
-Arouse (verb) - Canlandırmak, uyandırmak (bir fikri) - He succeeded in arousing the interest of the Dutch Ministry of Transport.
-Decisive (adj) - Belirleyici - That was a decisive moment in the history of bike-sharing
-Ingredient (noun) - Bileşen - A critical ingredient in success of hotels is developing and maintaining superior performance from their employees. 
-Accomplished (adj) - Başarılı - How is that accomplished? / Bu nasıl başarılır?
-Invest (verb) - Yatırım yapmak - What Human Resource Management (HRM) practices should organizations invest in to acquire and retain great employees?
-Retain (verb) - Sürdürmek - Yatırım yapmak - What Human Resource Management (HRM) practices should organizations invest in to acquire and retain great employees?
-Aim (verb) - Odaklanmak - Some hotels aim to provide superior working conditions for their employees.
-By contrast, (other) - Karşıt olarak - By contrast, the service sector, and more specifically hotels, has traditionally not extended these practices to address basic employee needs, such as good working conditions.
-In order to succeed (other) - Başarılı olmak için - Pfeffer emphasizes that in order to succeed in a global business environment, organizations must make investment in Human Resource Management.
-Substance (noun) - Öz - The substance of HRM practices 
-Recurring (adj) - Yinelenen - In addition, or maybe as a result, high employee turnover has been a recurring problem throughout the hospitality industry
-Succinctly (adv) - Kısaca - This was succinctly summarized by Michel
-Inherently (adv) - Doğal olarak - These conditions are inherently linked to the working environment.
-It is therefore (other) - Bu nedenle - It is therefore essential for hotel management to develop HRM practices that enable them to inspire and retain competent employees. 
-`;
+Drawback (noun) - Disadvantage or negative aspect of something - One possible drawback of using the internet for education is leading to copying homework.
+Enhance (verb) - Improve or increase the quality - Using the internet for education has a big advantage which lets students to enhance their studies.
+Taking precautions (verb) - Prevent potential harm or danger - To ensure our safety, we took precautions by wearing helmets and safety gear while riding our bikes.
+Remedy (noun) - Solution for a problem - There is no easy remedy for the environmental damage caused by industrial pollution.
+Fact (noun) - Piece of information that is true or can be proven  - The fact is, we can't afford to buy a new car right now.
+Narrow (verb, adj) - Making small - We need to narrow our focus and concentrate on the most important issues.
+Income (noun) - Money that person receives - he was pleased to receive a raise in her income after working at the company for a year.
+Growing concern (noun) - Situation when people are becoming worried or alarmed  - The widening gap between rich and poor is a growing concern in many countries around the world. 
+As a whole (noun) - Considering all parts of the aspect - This trend can have serious implications for the well-being of individuals and society as a whole.
+Well-being (noun) - Being comfortable, healthy, and happy - This trend can have serious implications for the well-being of individuals and society as a whole.
+Fund (noun) - Sum of money that is set aside for a specific purpose - The revenue generated from these taxes could then be used to fund programs and services for those in need.
+Wealth (noun) - Abundance of valuable resources / Zenginlik - When a small percentage of the population controls the majority of the wealth, it can create a sense of injustice and resentment among those who are less fortunate.
+Particularly (adv) - Specifically  -  Particularly when it came to publicizing Provo’s aims.
+Initiative (noun) - Ability to take action and make decisions independently / Girişim - The police were opposed to Provo’s initiatives and almost as soon as the white bikes were distributed around the city, they removed them.
+Seize (verb) - Take the advantage of an opportunity - Schimmelpennink seized this opportunity to present a more elaborate Witte Fietsenplan to the city council.
+Unanimously (adv) -Complete agreement or approval by all members - Nevertheless, the council unanimously rejected the plan.
+Arouse (verb) - Awaken a feeling or reaction in someone - He succeeded in arousing the interest of the Dutch Ministry of Transport.
+Decisive (adj) - Something that settles or determines an outcome - That was a decisive moment in the history of bike-sharing
+Ingredient (noun) - Component or part of a mixture - A critical ingredient in success of hotels is developing and maintaining superior performance from their employees. 
+Accomplished (adj) - Successful on achieving something - How is that accomplished? / Bu nasıl başarılır?
+Invest (verb) - Put money or resources into a project - What Human Resource Management (HRM) practices should organizations invest in to acquire and retain great employees?
+Retain (verb) - Keep possession or control something -  The company was able to retain its top employees by offering competitive salaries and benefits.
+Aim (noun) - Specific intention or purpose that someone has in mind - Some hotels aim to provide superior working conditions for their employees.
+By contrast, (other) - Highlight a difference or distinction between two things - By contrast, the service sector, and more specifically hotels, has traditionally not extended these practices to address basic employee needs, such as good working conditions.
+In order to succeed (other) - To be successful - Pfeffer emphasizes that in order to succeed in a global business environment, organizations must make investment in Human Resource Management.
+Substance (noun) - Essential part of something - The substance of HRM practices 
+Recurring (adj) - Something happens repeatedly - In addition, or maybe as a result, high employee turnover has been a recurring problem throughout the hospitality industry
+Succinctly (adv) - Express something in a clear and concise way - This was succinctly summarized by Michel
+Inherently (adv) - Naturally ? - These conditions are inherently linked to the working environment.
+It is therefore (other) - Logical conclusion / Bu nedenle - It is therefore essential for hotel management to develop HRM practices that enable them to inspire and retain competent employees. `;
 
 export default vocabularies;
